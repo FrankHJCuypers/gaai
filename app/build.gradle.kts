c215a8960cea21d2a18e0a7e3f6311349404cba7
Frank HJ Cuypers <frank@cuypers-ghys.be>
2024-10-31 20:31:22 +0100

    build(devtools): Use Dokka to generate Kdoc based documentation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,113 +1,116 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.jetbrains.kotlin.android)
-    alias(libs.plugins.compose.compiler)
-    alias(libs.plugins.room)
-    alias(libs.plugins.devtools.ksp)
+  alias(libs.plugins.android.application)
+  alias(libs.plugins.jetbrains.kotlin.android)
+  alias(libs.plugins.compose.compiler)
+  alias(libs.plugins.room)
+  alias(libs.plugins.devtools.ksp)
 
 }
 
 android {
-    namespace = "be.cuypers_ghys.gaai"
-    compileSdk = 35
+  namespace = "be.cuypers_ghys.gaai"
+  compileSdk = 35
 
-    defaultConfig {
-        applicationId = "be.cuypers_ghys.gaai"
-        minSdk = 26
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+  defaultConfig {
+    applicationId = "be.cuypers_ghys.gaai"
+    minSdk = 26
+    targetSdk = 35
+    versionCode = 1
+    versionName = "1.0"
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables {
-            useSupportLibrary = true
-        }
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    vectorDrawables {
+      useSupportLibrary = true
     }
+  }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
+  buildTypes {
+    release {
+      isMinifyEnabled = false
+      proguardFiles(
+        getDefaultProguardFile("proguard-android-optimize.txt"),
+        "proguard-rules.pro"
+      )
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+  }
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+  }
+  kotlinOptions {
+    jvmTarget = "1.8"
+  }
+  buildFeatures {
+    compose = true
+  }
+  composeOptions {
+    kotlinCompilerExtensionVersion = "1.5.1"
+  }
+  packaging {
+    resources {
+      excludes += "/META-INF/{AL2.0,LGPL2.1}"
     }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
+  }
 
-    // Needed for JUnit5 to work
-    @Suppress("UnstableApiUsage")
-    testOptions {
-        unitTests.all { it.useJUnitPlatform() }
-    }
+  // Needed for JUnit5 to work
+  @Suppress("UnstableApiUsage")
+  testOptions {
+    unitTests.all { it.useJUnitPlatform() }
+  }
 
-    room {
-        schemaDirectory("$projectDir/schemas")
-    }
+  room {
+    schemaDirectory("$projectDir/schemas")
+  }
 }
 
 dependencies {
 
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
-    implementation(libs.nordic.kotlin.ble.profile)
-    implementation(libs.nordic.kotlin.ble.scanner)
-    implementation(libs.nordic.kotlin.ble.client)
-    implementation(libs.cbor)
-    implementation(libs.accompanist.permissions)
-    implementation(libs.androidx.core.ktx)
+  implementation(libs.androidx.lifecycle.runtime.ktx)
+  implementation(libs.androidx.activity.compose)
+  implementation(platform(libs.androidx.compose.bom))
+  implementation(libs.androidx.ui)
+  implementation(libs.androidx.ui.graphics)
+  implementation(libs.androidx.ui.tooling.preview)
+  implementation(libs.androidx.material3)
+  implementation(libs.nordic.kotlin.ble.profile)
+  implementation(libs.nordic.kotlin.ble.scanner)
+  implementation(libs.nordic.kotlin.ble.client)
+  implementation(libs.cbor)
+  implementation(libs.accompanist.permissions)
+  implementation(libs.androidx.core.ktx)
 
-    implementation(libs.androidx.room.runtime)
-    implementation(libs.androidx.lifecycle.runtime.compose.android)
-    annotationProcessor(libs.androidx.room.compiler)
-    // To use Kotlin annotation processing tool (kapt)
-    //kapt(libs.androidx.room.compiler)
-    // To use Kotlin Symbol Processing (KSP)
-    ksp(libs.androidx.room.compiler)
-    implementation(libs.androidx.room.ktx)
+  implementation(libs.androidx.room.runtime)
+  implementation(libs.androidx.lifecycle.runtime.compose.android)
+  annotationProcessor(libs.androidx.room.compiler)
+  // To use Kotlin annotation processing tool (kapt)
+  //kapt(libs.androidx.room.compiler)
+  // To use Kotlin Symbol Processing (KSP)
+  ksp(libs.androidx.room.compiler)
+  implementation(libs.androidx.room.ktx)
 
-    // Include a slf4j implementation. libs.nordic.kotlin.ble uses it and without an implementation, no logging.
-    // Alternative is to include nordic-logger = { group = "no.nordicsemi.android.common", name = "logger", version.ref = "nordic-common" }?
-    // See Kotlin-BLE-Library, app_client packages, gradle script.
-    implementation(libs.slf4j.api)
-    implementation(libs.slf4j.simple)
+  // Include a slf4j implementation. libs.nordic.kotlin.ble uses it and without an implementation, no logging.
+  // Alternative is to include nordic-logger = { group = "no.nordicsemi.android.common", name = "logger", version.ref = "nordic-common" }?
+  // See Kotlin-BLE-Library, app_client packages, gradle script.
+  implementation(libs.slf4j.api)
+  implementation(libs.slf4j.simple)
 
 //    implementation(libs.androidx.room.common)
 
-    implementation(libs.androidx.navigation.runtime.ktx)
-    implementation(libs.androidx.navigation.compose)
-    implementation(libs.kotlinx.datetime)
-    implementation(libs.versioncompare)
+  implementation(libs.androidx.navigation.runtime.ktx)
+  implementation(libs.androidx.navigation.compose)
+  implementation(libs.kotlinx.datetime)
+  implementation(libs.versioncompare)
 
-    testImplementation(libs.junit)
-    testImplementation(libs.junit.jupiter)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
-    implementation(kotlin("script-runtime"))
+  testImplementation(libs.junit)
+  testImplementation(libs.junit.jupiter)
+  androidTestImplementation(libs.androidx.junit)
+  androidTestImplementation(libs.androidx.espresso.core)
+  androidTestImplementation(platform(libs.androidx.compose.bom))
+  androidTestImplementation(libs.androidx.ui.test.junit4)
+  debugImplementation(libs.androidx.ui.tooling)
+  debugImplementation(libs.androidx.ui.test.manifest)
+  implementation(kotlin("script-runtime"))
+
+  runtimeOnly("org.jetbrains.dokka:android-documentation-plugin:1.9.20")
 }
+
