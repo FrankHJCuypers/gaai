fad7d8c0054df507c152a4bc82aabebf1a0a26cf
Frank HJ Cuypers <frank@cuypers-ghys.be>
2025-09-04 18:53:40 +0200

    refactor: SDK_INT is always >= 26 due to minSdk=26

--- app/src/main/java/be/cuypers_ghys/gaai/ui/GaaiApp.kt
+++ app/src/main/java/be/cuypers_ghys/gaai/ui/GaaiApp.kt
@@ -122,17 +122,12 @@ fun adaptiveIconPainterResource(@DrawableRes id: Int): Painter {
   val res = LocalResources.current
   val theme = LocalContext.current.theme
 
-  return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-    // Android O supports adaptive icons, try loading this first (even though this is least likely to be the format).
-    val adaptiveIcon = ResourcesCompat.getDrawable(res, id, theme) as? AdaptiveIconDrawable
-    if (adaptiveIcon != null) {
-      BitmapPainter(adaptiveIcon.toBitmap().asImageBitmap())
-    } else {
-      // We couldn't load the drawable as an Adaptive Icon, just use painterResource
-      painterResource(id)
-    }
+   // Android O supports adaptive icons, try loading this first (even though this is least likely to be the format).
+  val adaptiveIcon = ResourcesCompat.getDrawable(res, id, theme) as? AdaptiveIconDrawable
+  return if (adaptiveIcon != null) {
+    BitmapPainter(adaptiveIcon.toBitmap().asImageBitmap())
   } else {
-    // We're not on Android O or later, just use painterResource
+    // We couldn't load the drawable as an Adaptive Icon, just use painterResource
     painterResource(id)
   }
 }
