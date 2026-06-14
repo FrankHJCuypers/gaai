b7132a2cb4a9ad38bc801930eaa01753aee54049
Frank HJ Cuypers <frank@cuypers-ghys.be>
2026-06-14 17:41:12 +0200

    build(deps): bumped dokka from 2.1.0 to 2.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
   alias(libs.plugins.room) apply false
   alias(libs.plugins.devtools.ksp) apply false
   // Generates HTML documentation
-  id("org.jetbrains.dokka") version "2.1.0"
+  id("org.jetbrains.dokka") version "2.2.0"
   id("com.google.gms.google-services") version "4.4.4" apply false
   id("com.google.firebase.crashlytics") version "3.0.6" apply false
 }
