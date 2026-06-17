18735b3119853c31929d8827235a26ba3028656c
Frank HJ Cuypers <frank@cuypers-ghys.be>
2026-06-17 16:44:06 +0200

    build(deps): The ‘apply’ plugin syntax is older and not recommended Applying a plugin with `apply` gives the following warning:   The `apply` plugin syntax is older and not recommended.

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,12 +7,7 @@ plugins {
   alias(libs.plugins.room) apply false
   alias(libs.plugins.devtools.ksp) apply false
   // Generates HTML documentation
-  id("org.jetbrains.dokka") version "2.2.0"
+  id("org.jetbrains.dokka") version "2.2.0" apply false
   id("com.google.gms.google-services") version "4.5.0" apply false
   id("com.google.firebase.crashlytics") version "3.0.7" apply false
 }
-
-subprojects {
-  apply(plugin = "org.jetbrains.dokka")
-}
-
